e9ec98b2c1c8ac68034a5dadd15c3c77409df7b8
quy_le_van <quy_le_van@bita.jp>
2022-11-14 22:52:48 +0700

    feat: login and register and reset pass

--- src/pages/auth/forgot-password/ForgotPassword.js
+++ src/pages/auth/forgot-password/ForgotPassword.js
@@ -1,33 +1,87 @@
-import './ForgotPassword.scss';
+import { useState } from 'react';
 import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import Input from '../../../components/input/input';
 import Button from '../../../components/button/Button';
-import { Link } from 'react-router-dom';
+import backgroundImage from '../../../assets/images/background.jpg';
+import './ForgotPassword.scss';
+import { authService } from '../../../services/api/auth/auth.service';
 
 const ForgotPassword = () => {
-  return (
-    <div className="auth-inner">
-      <form className="auth-form">
-        <div className="form-input-container">
-          <Input
-            id="password"
-            name="password"
-            type="password"
-            value="my password"
-            labelText="Password"
-            placeholder="Enter Password"
-            handleChange={() => {}}
-          />
-        </div>
-        <Button label={'SIGNUP'} className="auth-button button" disabled={true} />
+  const [email, setEmail] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [alertType, setAlertType] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
 
-        <Link to={'/'}>
-          <span className="forgot-password">
-            <FaArrowLeft className="arrow-left" />
-            Back to Login
-          </span>
-        </Link>
-      </form>
+  const forgotPassword = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+
+    try {
+      const response = await authService.forgotPassword(email);
+      setLoading(true);
+      setShowAlert(false);
+      setAlertType('alert-success');
+      setResponseMessage(response?.data?.message);
+    } catch (error) {
+      setLoading(false);
+      setAlertType('alert-error');
+      setShowAlert(true);
+      setResponseMessage(error?.response?.data.message);
+    }
+  };
+
+  return (
+    <div className="container-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="environment">DEV</div>
+      <div className="container-wrapper-auth">
+        <div className="tabs forgot-password-tabs" style={{ height: `${responseMessage}` ? '300px' : '' }}>
+          <div className="tabs-auth">
+            <ul className="tab-group">
+              <li className="tab">
+                <div className="login forgot-password">Forgot Password</div>
+              </li>
+            </ul>
+
+            <div className="tab-item">
+              <div className="auth-inner">
+                {responseMessage && (
+                  <div className={`alerts ${alertType}`} role="alert">
+                    {responseMessage}
+                  </div>
+                )}
+                <form className="forgot-password-form" onSubmit={forgotPassword}>
+                  <div className="form-input-container">
+                    <Input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={email}
+                      labelText="Password"
+                      placeholder="Enter Password"
+                      style={{ border: `${showAlert} ? '1px solid #fa9b8a'  : ''` }}
+                      handleChange={(event) => setEmail(event.target.value)}
+                    />
+                  </div>
+                  <Button
+                    label={`${loading ? 'FORGOT PASSWORD IN PROGRESS...' : 'FORGOT PASSWORD'}`}
+                    className="auth-button button"
+                    disabled={!email}
+                  />
+
+                  <Link to={'/'}>
+                    <span className="login">
+                      <FaArrowLeft className="arrow-left" />
+                      Back to Login
+                    </span>
+                  </Link>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -1,41 +1,97 @@
-import './Register.scss';
+import { useState, useEffect } from 'react';
 import Input from '../../../components/input/input';
 import Button from '../../../components/button/Button';
+import './Register.scss';
+import { Utils } from '../../../services/utils/utils.service';
+import { authService } from '../../../services/api/auth/auth.service';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [alertType, setAlertType] = useState('');
+  const [hasError, setHasError] = useState(false);
+  const [user, setUser] = useState();
+
+  const registerUser = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+    try {
+      const avatarColor = Utils.avatarColor();
+      const avatarImage = Utils.generateAvatar(username.charAt(0).toUpperCase(), avatarColor);
+      const result = await authService.signUp({
+        username,
+        email,
+        password,
+        avatarColor,
+        avatarImage
+      });
+      setUser(result.data.user);
+      setHasError(false);
+      setAlertType('alert-success');
+    } catch (error) {
+      setLoading(false);
+      setHasError(true);
+      setAlertType('alert-error');
+      setErrorMessage(error?.response?.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (loading && !user) return;
+    if (user) {
+      console.log('navigate to streams page');
+      setLoading(false);
+    }
+  }, [loading, user]);
+
   return (
     <div className="auth-inner">
-      <form className="auth-form">
+      {hasError && errorMessage && (
+        <div className={`alerts ${alertType}`} role="alert">
+          {errorMessage}
+        </div>
+      )}
+      <form className="auth-form" onSubmit={registerUser}>
         <div className="form-input-container">
           <Input
             id="username"
             name="username"
             type="text"
-            value="thi"
+            value={username}
             labelText="Username"
             placeholder="Enter Username"
-            handleChange={() => {}}
+            style={{ border: `${hasError} ? '1px solid #fa9b8a'  : ''` }}
+            handleChange={(event) => setUsername(event.target.value)}
           />
           <Input
             id="email"
             name="email"
             type="email"
-            value="quy0694@gmail.com"
+            value={email}
             labelText="Email"
             placeholder="Enter Email"
-            handleChange={() => {}}
+            style={{ border: `${hasError} ? '1px solid #fa9b8a'  : ''` }}
+            handleChange={(event) => setEmail(event.target.value)}
           />
           <Input
             id="password"
             name="password"
             type="password"
-            value="my password"
+            value={password}
             labelText="Password"
             placeholder="Enter Password"
-            handleChange={() => {}}
+            style={{ border: `${hasError} ? '1px solid #fa9b8a'  : ''` }}
+            handleChange={(event) => setPassword(event.target.value)}
           />
         </div>
-        <Button label={'SIGNUP'} className="auth-button button" disabled={true} />
+        <Button
+          label={`${loading ? 'SIGNUP IN PROGRESS...' : 'SIGNUP'}`}
+          className="auth-button button"
+          disabled={!username || !email || !password}
+        />
       </form>
     </div>
   );
